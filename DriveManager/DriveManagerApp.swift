024abd51b8ef3760f15fb9241978e3b8e1238de9
Sean <smandable@gmail.com>
2026-03-06 13:14:32 -0500

    Initial Commit

---
+++ DriveManager/DriveManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveManagerApp.swift
+//  DriveManager
+//
+//  Created by Sean Mandable on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DriveManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
